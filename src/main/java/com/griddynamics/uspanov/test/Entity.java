b153b7b9e594c4f18f191d7fc66d6be31734fe1b
NailUspanov <nailuspnov@gmail.com>
2021-12-28 18:22:22 +0400

    EntityManager-2

---
+++ src/main/java/com/griddynamics/uspanov/test/Entity.java
@@ -0,0 +1,16 @@
+package com.griddynamics.uspanov.test;
+
+public class Entity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    private Long age;
+}
+
--- src/main/java/com/griddynamics/uspanov/test/EntityManager.java
+++ src/main/java/com/griddynamics/uspanov/test/EntityManager.java
@@ -5,25 +5,21 @@ import com.github.javafaker.Faker;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
-import java.util.function.BiConsumer;
+import java.util.function.Function;
 
-public class EntityManager<T> {
+public class EntityManager {
     private final Faker faker = new Faker();
-    private final Map<String, BiConsumer<Field, T>> map = Map.of("Long", (field, obj) -> {
-        try {
-            field.set(obj, faker.number().randomNumber());
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    });
+    private final Map<String, Function<Field, Object>> map = Map.of("Long", field -> faker.number().randomNumber(),
+            "String", field -> faker.animal().name());
 
-
-    T create(Class<T> type) throws NoSuchMethodException,
+    public <T> T create(Class<T> type) throws NoSuchMethodException,
                                     InvocationTargetException, InstantiationException, IllegalAccessException {
-        T object = type.getConstructor().newInstance();
+        T obj = type.getConstructor().newInstance();
         for (Field field: type.getDeclaredFields()){
-            map.get(field.getType().getName()).accept(field, object);
+            field.setAccessible(true);
+            field.set(obj, map.get(field.getType().getName().split("\\.")[2]).apply(field));
+            field.setAccessible(false);
         }
-        return object;
+        return obj;
     }
 }
