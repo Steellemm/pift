5a66dbe8d829f3c5071120aaacca8af38d353362
NailUspanov <nailuspnov@gmail.com>
2021-12-29 23:32:09 +0400

    EntityManager-5

--- src/main/java/com/griddynamics/uspanov/test/Entity.java
+++ src/main/java/com/griddynamics/uspanov/test/Entity.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
 
 @Setter
 @Getter
 @javax.persistence.Entity
+@Table(name = "entity_test")
 public class Entity {
     @Column
     private String name;
