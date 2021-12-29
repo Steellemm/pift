37836f11bab35e532bac9dbab74c1711f466c08c
NailUspanov <nailuspnov@gmail.com>
2021-12-29 18:07:39 +0400

    EntityManager-4

--- src/main/java/com/griddynamics/uspanov/test/EntityManager.java
+++ src/main/java/com/griddynamics/uspanov/test/EntityManager.java
@@ -1,20 +1,61 @@
 package com.griddynamics.uspanov.test;
 
-import com.github.javafaker.Faker;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
+import javax.persistence.Column;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.util.Map;
-import java.util.function.Function;
+import java.util.*;
 
 public class EntityManager {
-    private final Faker faker = new Faker();
-    private final Map<String, Function<Field, Object>> map = Map.of("Long", field -> faker.number().randomNumber(),
-            "String", field -> faker.animal().name(),
-            "Integer", field -> faker.number().numberBetween(Integer.MIN_VALUE, Integer.MAX_VALUE),
-            "BigDecimal", field -> new BigDecimal(faker.number().randomNumber()));
+    private static final Sql2o dataSource = new Sql2o
+            ("jdbc:postgresql://localhost:5432/test_db", "postgres", "postgres");
+    private final List<Object> entityList = new ArrayList<>();
 
     public <T> T create(Class<T> type) {
-        return EntityUtils.create(type, map);
+        T obj = EntityUtils.create(type);
+        entityList.add(obj);
+        return obj;
     }
+
+    public void flush() {
+        entityList.forEach(this::saveEntity);
+        entityList.clear();
+    }
+
+    private void saveEntity(Object entity) {
+        String insertQuery = "insert into entity_test values (:name, :number, :age, :count)";
+        Class<?> type = entity.getClass();
+        Map<String, Object> entityMap = new HashMap<>();
+        Arrays.stream(type.getDeclaredFields())
+                .forEach(field -> entityMap.put(getColumnName(field), readField(field, entity)));
+        try (Connection con = dataSource.open()) {
+            con.createQuery(insertQuery)
+                    .addParameter("name", (String) entityMap.get("name"))
+                    .addParameter("number", (Long) entityMap.get("number"))
+                    .addParameter("age", (Integer) entityMap.get("age"))
+                    .addParameter("count", (BigDecimal) entityMap.get("count"))
+                    .executeUpdate();
+        }
+    }
+
+    private String getColumnName(Field field) {
+        if (field.isAnnotationPresent(javax.persistence.Column.class)) {
+            System.out.println(field.getAnnotation(Column.class).name());
+            return field.getAnnotation(Column.class).name();
+        }
+        System.out.println(field.getName());
+        return field.getName();
+    }
+
+    private Object readField(Field field, Object target) {
+        try {
+            return FieldUtils.readField(field, target, true);
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
 }
--- src/main/java/com/griddynamics/uspanov/test/Main.java
+++ src/main/java/com/griddynamics/uspanov/test/Main.java
@@ -4,9 +4,6 @@ public class Main {
     public static void main(String[] args) {
         EntityManager entityManager = new EntityManager();
         Entity entity = entityManager.create(Entity.class);
-        System.out.println("Age " + entity.getAge());
-        System.out.println("Count " + entity.getCount());
-        System.out.println("Name " + entity.getName());
-        System.out.println("Number " + entity.getNumber());
+        entityManager.flush();
     }
 }
