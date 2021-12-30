a7a69676a705c97040fe7a119490c0f4539239d9
NailUspanov <nailuspnov@gmail.com>
2021-12-30 13:57:13 +0400

    EntityManager-7

--- src/main/java/com/griddynamics/uspanov/test/Main.java
+++
@@ -1,9 +0,0 @@
-package com.griddynamics.uspanov.test;
-
-public class Main {
-    public static void main(String[] args) {
-        EntityManager entityManager = new EntityManager();
-        Entity entity = entityManager.create(Entity.class);
-        entityManager.flush();
-    }
-}
