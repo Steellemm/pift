337b1bccdcec86072cc1a762fcce8cef01dcde9e
NailUspanov <nailuspnov@gmail.com>
2022-01-15 15:07:50 +0400

    PIFT-1: Code coverage with unit tests

--- src/main/java/com/griddynamics/pift/EntityManager.java
+++ src/main/java/com/griddynamics/pift/EntityManager.java
@@ -3,8 +3,12 @@ package com.griddynamics.pift;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Slf4j
@@ -15,17 +19,31 @@ public class EntityManager {
     private final String USER;
     private final String PASSWORD;
 
-    public void flush() {
-        createdEntitiesList.forEach(this::saveEntity);
+    public int flush() {
+        AtomicInteger rowsAffectedCount = new AtomicInteger(0);
+        createdEntitiesList.forEach(x -> rowsAffectedCount.addAndGet(saveEntity(x)));
+        log.debug(String.valueOf(rowsAffectedCount.get()));
         createdEntitiesList.clear();
+        return rowsAffectedCount.get();
     }
 
     public <T> T create(Class<T> type) {
         return EntityUtils.create(type, createdEntitiesList);
     }
 
-    private void saveEntity(Object entity) {
-        SQLUtils.connect(URL, USER, PASSWORD, entity);
+    private int saveEntity(Object entity) {
+        return executeInsertQuery(entity);
+    }
+
+    private int executeInsertQuery(Object entity){
+            String insertQuery = SQLUtils.createQueryForInsert(entity);
+            log.debug(insertQuery);
+            try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+                 Statement stmt = con.createStatement()) {
+                return stmt.executeUpdate(insertQuery);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Exception in connect method", e);
+            }
     }
 
 }
