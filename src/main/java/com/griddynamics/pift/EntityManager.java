6f32a4b7a8fa32ebab6183eb94255f8abdcad4c3
NailUspanov <nailuspnov@gmail.com>
2022-01-15 20:06:01 +0400

    PIFT-2: Documentation

--- src/main/java/com/griddynamics/pift/EntityManager.java
+++ src/main/java/com/griddynamics/pift/EntityManager.java
@@ -3,8 +3,12 @@ package com.griddynamics.pift;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Slf4j
@@ -15,17 +19,49 @@ public class EntityManager {
     private final String USER;
     private final String PASSWORD;
 
-    public void flush() {
-        createdEntitiesList.forEach(this::saveEntity);
+    /***
+     * Pushes the objects from createdEntitiesList into database and then clears the list.
+     * @return int number of affected rows in database.
+     */
+    public int flush() {
+        AtomicInteger rowsAffectedCount = new AtomicInteger(0);
+        createdEntitiesList.forEach(x -> rowsAffectedCount.addAndGet(saveEntity(x)));
+        log.debug(String.valueOf(rowsAffectedCount.get()));
         createdEntitiesList.clear();
+        return rowsAffectedCount.get();
     }
 
+    /***
+     * Creates new instance of type class with random values in fields.
+     * @param type of the entity class.
+     * @return new instance of type class.
+     */
     public <T> T create(Class<T> type) {
         return EntityUtils.create(type, createdEntitiesList);
     }
 
-    private void saveEntity(Object entity) {
-        SQLUtils.connect(URL, USER, PASSWORD, entity);
+    /***
+     * Saves received object to the database.
+     * @param entity object
+     * @return int number of affected rows in database.
+     */
+    private int saveEntity(Object entity) {
+        return executeQuery(SQLUtils.createQueryForInsert(entity));
+    }
+
+    /***
+     * Executes a query to the database.
+     * @param query
+     * @return int number of affected rows in database.
+     */
+    private int executeQuery(String query){
+            log.debug(query);
+            try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+                 Statement stmt = con.createStatement()) {
+                return stmt.executeUpdate(query);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Exception in connect method", e);
+            }
     }
 
 }
--- src/main/java/com/griddynamics/pift/SQLUtils.java
+++ src/main/java/com/griddynamics/pift/SQLUtils.java
@@ -1,30 +1,41 @@
 package com.griddynamics.pift;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.FieldUtils;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import java.lang.reflect.Field;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 import java.util.Arrays;
 
 @Slf4j
 public class SQLUtils {
-    public static void connect(String url, String user, String password, Object entity) {
-        String insertQuery = createQueryForInsert(entity);
-        log.debug(insertQuery);
-        try (Connection con = DriverManager.getConnection(url, user, password);
-             Statement stmt = con.createStatement()) {
-            stmt.executeUpdate(insertQuery);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Exception in connect method", e);
+
+    /***
+     * Returns the field value valid for the request.
+     * @param field to be read.
+     * @param target object to be read from.
+     * @return String value of field.
+     */
+    public static String readField(Field field, Object target) {
+        try {
+            Object o = FieldUtils.readField(field, target, true);
+            if (o instanceof String) {
+                return "'" + o + "'";
+            }
+            return o.toString();
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
         }
     }
 
-    private static String createQueryForInsert(Object entity) {
+    /***
+     * Creates query for insert data into database.
+     * @param entity object.
+     * @return String query
+     */
+    public static String createQueryForInsert(Object entity) {
         Class<?> type = entity.getClass();
         StringBuilder insertQuery =
                 new StringBuilder("INSERT INTO ")
@@ -38,21 +49,31 @@ public class SQLUtils {
             }
             if (field.isAnnotationPresent(JoinColumn.class)) {
                 values.append(
-                        ReflectionUtils.readField
-                                (getIdField(entity, field), ReflectionUtils.getFieldValue(field, entity))
+                        readField(getIdField(entity, field), ReflectionUtils.getFieldValue(field, entity))
                 );
-            } else values.append(ReflectionUtils.readField(field, entity));
+            } else values.append(readField(field, entity));
             insertQuery.append(getColumnName(field));
         });
         return insertQuery.append(") values (").append(values).append(")").toString();
     }
 
+    /***
+     * Gets id field from object reference field.
+     * @param entity object.
+     * @param field that references another object.
+     * @return id field
+     */
     private static Field getIdField(Object entity, Field field) {
         return Arrays.stream(ReflectionUtils.getFieldValue(field, entity).getClass()
                         .getDeclaredFields()).filter(x -> x.isAnnotationPresent(Id.class))
                 .findFirst().orElseThrow(() -> new IllegalArgumentException("Exception in getIdField method"));
     }
 
+    /***
+     * Gets the table column name that matches the received field.
+     * @param field to be matched.
+     * @return String name of column.
+     */
     private static String getColumnName(Field field) {
         if (field.isAnnotationPresent(JoinColumn.class)) {
             return field.getAnnotation(JoinColumn.class).name();
