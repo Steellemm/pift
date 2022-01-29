67a063482b62da5f959d3ede8fac91a3903394fe
nuspanov <nailuspnov@gmail.com>
2022-01-29 13:53:07 +0400

    PIFT-8: Add yml file

--- src/main/java/com/griddynamics/pift/FieldCreatorManager.java
+++ src/main/java/com/griddynamics/pift/FieldCreatorManager.java
@@ -29,9 +29,10 @@ import java.util.function.Function;
 public class FieldCreatorManager {
     private static final Faker faker = new Faker();
     private static final ObjectMapper MAPPER = new ObjectMapper(new YAMLFactory());
-    private static final Map<Field, CreatorFunction> userCreatorByField = new HashMap<>();
-    private static PiftProperties piftProperties;
+    private final Map<Field, CreatorFunction> userCreatorByField = new HashMap<>();
+    private final PiftProperties piftProperties;
 
+    //TODO
     private final Map<FieldType, FieldCreator> fieldCreatorMap = Map.of(
             FieldType.STRING, new StringFieldCreator()
     );
@@ -41,7 +42,7 @@ public class FieldCreatorManager {
      * where key - class of field type
      * value - lambda that generates value for this type
      */
-    private static final Map<Class<?>, Function<Field, Object>> fieldsMapping = Map.of(
+    private final Map<Class<?>, Function<Field, Object>> fieldsMapping = Map.of(
             Long.class, field -> faker.number().randomNumber(),
             String.class, field -> faker.animal().name(),
             Integer.class, field -> faker.number().numberBetween(Integer.MIN_VALUE, Integer.MAX_VALUE),
@@ -53,10 +54,6 @@ public class FieldCreatorManager {
     );
 
     public FieldCreatorManager() {
-        setYaml();
-    }
-
-    private static void setYaml() {
         try {
             piftProperties = MAPPER.readValue(new File("pift.yaml"), PiftProperties.class);
         } catch (IOException e) {
@@ -68,27 +65,31 @@ public class FieldCreatorManager {
         if (userCreatorByField.containsKey(field)) {
             return userCreatorByField.get(field).apply(field);
         }
-        if (existInProperties(field).isPresent()) {
+        if (getFromProperties(field).isPresent()) {
             if (existInForeignKeys(field).isPresent()){
                 return createdEntitiesList.stream()
                         .filter(obj -> ReflectionUtils.getTableName(obj.getClass())
                                 .equals(existInForeignKeys(field).orElse("")))
                         .findFirst().orElseThrow(() -> new IllegalArgumentException("FK object has not been created yet"));
             }
-            return fieldCreatorMap.get(existInProperties(field).get().getType())
-                    .createValue(field, existInProperties(field).get());
+            return fieldCreatorMap.get(getFromProperties(field).get().getType())
+                    .createValue(field, getFromProperties(field).get());
         }
         return fieldsMapping.get(field.getType()).apply(field);
     }
 
-    private Optional<Column> existInProperties(Field field) {
-        if (piftProperties.getTables().containsKey(ReflectionUtils.getTableName(field))
-                && piftProperties.getTables().get(ReflectionUtils.getTableName(field))
-                .getColumns().containsKey(SQLUtils.getColumnName(field))) {
+    private Optional<Column> getFromProperties(Field field) {
+        if (existInProperties(field)) {
             return Optional.ofNullable(piftProperties.getTables().get(ReflectionUtils.getTableName(field))
                     .getColumns().get(SQLUtils.getColumnName(field)));
         }
         return Optional.empty();
+    }
+
+    private boolean existInProperties(Field field) {
+        return piftProperties.getTables().containsKey(ReflectionUtils.getTableName(field))
+                && piftProperties.getTables().get(ReflectionUtils.getTableName(field))
+                .getColumns().containsKey(SQLUtils.getColumnName(field));
     }
 
     private Optional<String> existInForeignKeys(Field field) {
@@ -101,15 +102,15 @@ public class FieldCreatorManager {
         return Optional.empty();
     }
 
-    public static void addValueGenerator(Class<?> type, Function<Field, Object> generator) {
+    public void addValueGenerator(Class<?> type, Function<Field, Object> generator) {
         fieldsMapping.put(type, generator);
     }
 
-    public static void addValueGenerator(Field field, CreatorFunction creatorFunction) {
+    public void addValueGenerator(Field field, CreatorFunction creatorFunction) {
         userCreatorByField.put(field, creatorFunction);
     }
 
-    public static boolean contains(Class<?> type) {
+    public boolean contains(Class<?> type) {
         return fieldsMapping.containsKey(type);
     }
 }
